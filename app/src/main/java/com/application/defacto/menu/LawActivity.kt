d48f954d87bdd4d3c112030f0bf511c09ebb3a02
Muhammad Nur Yusri Maulidin Yusuf <naoruyuzuri@gmail.com>
2021-05-21 02:00:44 +0800

    adding new empty activity

---
+++ app/src/main/java/com/application/defacto/menu/LawActivity.kt
@@ -0,0 +1,12 @@
+package com.application.defacto.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.application.defacto.R
+
+class LawActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_law)
+    }
+}
