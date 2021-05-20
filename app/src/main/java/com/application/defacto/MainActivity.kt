8e938181910f6034976342b5ef2e02359750a919
Muhammad Nur Yusri Maulidin Yusuf <naoruyuzuri@gmail.com>
2021-05-21 01:58:56 +0800

    updating few codes

--- app/src/main/java/com/application/defacto/MainActivity.kt
+++ app/src/main/java/com/application/defacto/MainActivity.kt
@@ -2,10 +2,16 @@ package com.application.defacto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.application.defacto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.bottomNav.menu.getItem(2).isEnabled = false
     }
 }
